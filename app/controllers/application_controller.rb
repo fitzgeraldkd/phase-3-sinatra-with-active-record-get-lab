a7660a9a619994ffae5d15e0589fe45821a189d2
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-10 15:22:00 -0500

    commit tree 3a87adb8ac1d3bbdb848b184626142588ef4f5ec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    Bakery.all.to_json
+  end
+
+  get '/bakeries/:id' do
+    Bakery.find(params[:id]).to_json(
+      include: :baked_goods
+    )
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood.order(price: :desc).to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    BakedGood.all.max {|a, b| a.price <=> b.price }.to_json
+  end
 
 end
